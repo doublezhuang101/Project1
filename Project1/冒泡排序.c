92a2761e368171e03e1fb1f44df90b2d20c893ec
壮 苗 <doublezhuang101@hotmail.com>
2019-01-14 14:28:45 +0800

    there is something wrong with swap

--- Project1/冒泡排序.c
+++ Project1/冒泡排序.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-
+int swap(int x, int y);
 int main()
 {
 	int a[] = { 15,46,41,31,4,48,45,7,8 };
 	int i, j, flag = 0;
 	int n;
+	int c;
 	n = sizeof(a) / sizeof(a[0]);
-	printf("%d", n);
+	printf("%d\n", n);
 	for (i = n - 1; i > 0; i--)
 	{
 		flag = 0;
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < i; j++)
 		{
 			if (a[j] > a[j + 1])
 			{
-				swap(a[j], a[j + 1]);
+				/*swap(a[j], a[j + 1]);*/
+				c = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = c;
 				flag = 1;
 			}
 		}
 		if (flag == 0) break;
 	}
-	
+	for (i = 0; i < n; i++)
+		printf("%d ", a[i]);
+}
+int swap(int x, int y)
+{
+	int c;
+	c = x;
+	x= y;
+	y = c;
+	return(x, y);
 }
