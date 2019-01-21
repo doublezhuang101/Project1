364eab2943d0576898fa53b8fa28986f9c685712
壮 苗 <doublezhuang101@hotmail.com>
2019-01-22 00:17:21 +0800

    finished

--- Project1/6.4.c
+++ Project1/6.4.c
@@ -2,22 +2,25 @@
 #include<string.h>
 int main()
 {
-	int a[] = { 1,7,87,9,78,798,54,64 };
+	int a[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int n=0,count=0,i=0;
 	count = sizeof(a) / sizeof(a[0]);
 	printf("please input a number:");
 	scanf("%d", &n);
 	for (i=0; i < count; i++);
-	{
-		if (a[i] == n)
-			a[i] = -1;
+	{	
+		if (n == a[i])
+			;
 	}
-	for (i = 0; i < count; i++)
+	if (n == 10)
+		printf("Not exists!\n");
+	else
 	{
-		printf("%4d", a[i]);
-		if (a[i] == -1)
-			printf("\b\b\b\b");
+		for (i = n; i < count; i++)
+			a[i-1] = a[i ];
+		for (i = 0; i < (count-1); i++)
+			printf("%3d", a[i]);
 	}
-	printf("1111\n");
+	printf("\n");
 	return 0;
 }
